c41e6a3d528712297311b1a5932ab278da9677aa
Marcellus Jones <mjones@Marcellus-Joness-MBP.local>
2019-08-01 10:05:30 -0700

    push

--- app/public/js/all.js
+++ app/public/js/all.js
@@ -15,24 +15,24 @@ $.get("/api/all", function(data) {
    // Now  we add our score data to the well we just placed on the page
   //  $('table tr td' + i span).html(data[i].note);
    
-   $("#score-well-" + i).append("<tr><td>Web Team representative for escalation chosen: " + data[i].note + "</td></tr>");
-   $("#score-well-" + i).append("<p>Content/CMS Freeze (EoD): " + data[i].note1 + "</p>");
-   $("#score-well-" + i).append("<p>Complete rollback package from master + TDS sync (11AM completion): " + data[i].note2 + "</p>");
-   $("#score-well-" + i).append("<p>Validate freeze was adhered to after TDS sync: " + data[i].note3 + "</p>");
-   $("#score-well-" + i).append("<p>Deploy rollback package to QA, and test it (2PM completion) (If all goes well, we won't need this package, but it must be tested in case release package breaks in Production.): " + data[i].note4 + "</p>");
-   $("#score-well-" + i).append("<p>In subtask of the CHG identify yourself as the escalation person if there are any issues with the release.: " + data[i].note5 + "</p>");
-   $("#score-well-" + i).append("<p>QA Certify release package (CoB): " + data[i].note6 + "</p>");
-   $("#score-well-" + i).append("<p>Enter Master & Release package version numbers in the Implementation Tasks field of the RBR (Don't add time): " + data[i].note7 + "</p>");
-   $("#score-well-" + i).append("<p>Deploy release package to Production CM, and test it (before going home): " + data[i].note8 + "</p>");
-   $("#score-well-" + i).append("<p>Generate QA certification.: " + data[i].note9 + "</p>");
-   $("#score-well-" + i).append("<p>Released on time: " + data[i].note10 + "</p>");
-   $("#score-well-" + i).append("<p>Rolled back?: " + data[i].note11 + "</p>");
-   $("#score-well-" + i).append("<p>Web team PPT features/bug fixes and replies to all: " + data[i].note12 + "</p>");
-   $("#score-well-" + i).append("<p>Tag release, merge release to master, and merge master to develop: " + data[i].note13 + "</p>");
-   $("#score-well-" + i).append("<p>Date: " + data[i].shell + "</p>");
-   $("#score-well-" + i).append("<p>Release_name: " + data[i].release_Name + "</p>");
-   $("#score-well-" + i).append("<p>Sprint_master: " + data[i].sprint_Master + "</p>");
-   $("#score-well-" + i).append("<p>Score: " + data[i].score + "</p>");
+  //  $("#score-well-" + i).append("<tr><td>Web Team representative for escalation chosen: " + data[i].note + "</td></tr>");
+  //  $("#score-well-" + i).append("<p>Content/CMS Freeze (EoD): " + data[i].note1 + "</p>");
+  //  $("#score-well-" + i).append("<p>Complete rollback package from master + TDS sync (11AM completion): " + data[i].note2 + "</p>");
+  //  $("#score-well-" + i).append("<p>Validate freeze was adhered to after TDS sync: " + data[i].note3 + "</p>");
+  //  $("#score-well-" + i).append("<p>Deploy rollback package to QA, and test it (2PM completion) (If all goes well, we won't need this package, but it must be tested in case release package breaks in Production.): " + data[i].note4 + "</p>");
+  //  $("#score-well-" + i).append("<p>In subtask of the CHG identify yourself as the escalation person if there are any issues with the release.: " + data[i].note5 + "</p>");
+  //  $("#score-well-" + i).append("<p>QA Certify release package (CoB): " + data[i].note6 + "</p>");
+  //  $("#score-well-" + i).append("<p>Enter Master & Release package version numbers in the Implementation Tasks field of the RBR (Don't add time): " + data[i].note7 + "</p>");
+  //  $("#score-well-" + i).append("<p>Deploy release package to Production CM, and test it (before going home): " + data[i].note8 + "</p>");
+  //  $("#score-well-" + i).append("<p>Generate QA certification.: " + data[i].note9 + "</p>");
+  //  $("#score-well-" + i).append("<p>Released on time: " + data[i].note10 + "</p>");
+  //  $("#score-well-" + i).append("<p>Rolled back?: " + data[i].note11 + "</p>");
+  //  $("#score-well-" + i).append("<p>Web team PPT features/bug fixes and replies to all: " + data[i].note12 + "</p>");
+  //  $("#score-well-" + i).append("<p>Tag release, merge release to master, and merge master to develop: " + data[i].note13 + "</p>");
+   $("#score-well-" + i).append("<th>Date: " + data[i].shell + "</th>");
+   $("#score-well-" + i).append("<th>Release_name: " + data[i].release_Name + "</th>");
+   $("#score-well-" + i).append("<th>Sprint_master: " + data[i].sprint_Master + "</th>");
+   $("#score-well-" + i).append("<th>Score: " + data[i].score + "</th>");
 
 
    console.log(data);
