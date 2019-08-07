a052f4affbb3f89c5ed2e83a2f75ecd0e5b98c2a
Marcellus Jones <mjones@Marcellus-Joness-MBP.local>
2019-08-06 17:12:19 -0700

    changes to html-routes

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -22,15 +22,19 @@ module.exports = function(app) {
   app.get("/all", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/all.html"));
   });
-
+  app.get("/api/"+ data.id, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/view.html"));
+  });
   // short route loads the short.html page, where short books in the db are displayed
   app.get("/low", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/low.html"));
   });
 
   // long route loads the long.html page, where long books in the db are displayed
-  app.get("/test", function(req, res) {
+  app.get("/test/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/comp.html"));
   });
-
+  // app.get("/test/:id", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/comp.html"));
+  // });
 };
