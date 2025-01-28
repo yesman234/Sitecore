8e334b756d5ae6e723396ad60e6797f32d9b8f99
Marcellus Devon Jones <mdjonesonly310@gmail.com>
2025-01-28 08:24:09 -0800

    port update paid render hosting

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var express = require("express");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8000;
+var PORT = process.env.PORT || 10000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
