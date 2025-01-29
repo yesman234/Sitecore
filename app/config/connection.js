82aff0e14c1862dc83489caea961276bd71648ed
Marcellus Devon Jones <mdjonesonly310@gmail.com>
2025-01-29 14:09:22 -0800

    render port change 3307

--- app/config/connection.js
+++ app/config/connection.js
@@ -5,7 +5,7 @@ var Sequelize = require("sequelize");
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
 var sequelize = new Sequelize("sitecore_library", "root", "password", {
   // change port # alter user w/ preveliges flushed
-  port: 3306,
+  port: 3307,
   ip: "127.0.0.1",
   user: "root",
   password: "password",
