efac8b9093200a4710bd68696c037d0060ed92e8
Marcellus Devon Jones <mdjonesonly310@gmail.com>
2025-01-28 16:32:39 -0800

    render yaml

--- app/config/connection.js
+++ app/config/connection.js
@@ -16,18 +16,5 @@ var sequelize = new Sequelize("sitecore_library", "root", "password", {
   }
 });
 
-
 // // Exports the connection for other files to use
 module.exports = sequelize;
-// var mysql = require('mysql');
-// var connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-// connection.connect();
-
-// connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-//   if (err) throw err;
-
-//   console.log('The solution is: ', rows[0].solution);
-// });
-
-// connection.end();
