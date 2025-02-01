04ca0d71bf7c018b80e817cab7c52049756350cc
Marcellus Devon Jones <mdjonesonly310@gmail.com>
2025-01-31 16:37:18 -0800

    yaml

--- app/config/connection.js
+++ app/config/connection.js
@@ -3,11 +3,11 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("sitecore_library", "root", "password", {
+var sequelize = new Sequelize("sitecore_library", "mysql-2f4b", "7zjQZqzy2YXp6bpIego3mMZvJgZYhHusYwgh87SYUn4=", {
   // change port # alter user w/ preveliges flushed
-  port: 3306,
-  user: "root",
-  password: "password",
+  port: "",
+  user: "mysql",
+  password: "R2o022Pv+PAXJa6L3HCA8W7uwlYhZjzoOXxNNoxwGNU=",
   dialect: "mysql",
   pool: {
     max: 20,
