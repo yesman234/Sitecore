483abaf67b3523e1d61ba2efec7a24245b4f012b
Marcellus Devon Jones <mdjonesonly310@gmail.com>
2025-02-06 15:57:17 -0800

    error connect

--- app/config/connection.js
+++ app/config/connection.js
@@ -2,9 +2,12 @@
 // Dependencies
 var Sequelize = require("sequelize");
 
+// with database, username, and password in the options object
+//Server=localhost,1433;Initial Catalog=master;User Id=sa;Password=password;TrustServerCertificate=true
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
 var sequelize = new Sequelize("sitecore_library", "root", "password", {
   // change port # alter user w/ preveliges flushed
+  ip:"127.0.0.1",
   port: 3306,
   dialect: "mysql",
   pool: {
@@ -13,6 +16,15 @@ var sequelize = new Sequelize("sitecore_library", "root", "password", {
     idle: 10000
   }
 });
+ 
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('DB connection established successfully')
+    })
+    .catch(err => {
+        console.log('Unable to connect to DB', err);
+    });
 
 // // Exports the connection for other files to use
 module.exports = sequelize;
